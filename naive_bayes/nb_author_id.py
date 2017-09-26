5b1d21cabc7f86da44a8b023a0ff02cdc9dca3b6
anonyms <stefanjonski@gmail.com>
2017-09-26 22:35:43 +0200

    Added fitting and prediction function with Naive Bayes

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -9,7 +9,7 @@
     Sara has label 0
     Chris has label 1
 """
-    
+import numpy   
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -21,11 +21,30 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+print "here goes nothing"
+print features_train
 
 
 
 #########################################################
 ### your code goes here ###
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
+
+t0 = time()
+clf.fit(features_train,labels_train)
+print "fitting time:", round(time()-t0, 3), "s"
+
+t1 = time()
+pred = clf.predict(features_test)
+print "predicting time:",round(time()-t1,3),"s"
+
+t2 = time()
+accuracy = clf.score(features_test,labels_test)
+print "scoring time:",round(time()-t2,3),"s"
+
+print accuracy
+
 
 
 #########################################################
